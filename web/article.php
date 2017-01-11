715c4d5c507a49912a7c8f8075b5a0dc33b45f1f
Dorian Bonnamy <dorian.bonnamy@gmail.com>
2017-01-11 16:48:14 +0100

    Ajout de la page article.php, welcome.php. Du sous-dossier css qui contiendra tous les fichiers css

---
+++ web/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>Article</title>
+		<script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
+		<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
+		<link rel="stylesheet" href="css/article.css"/>
+
+	</head>
+	<body>
+
+		<textarea>
+
+		</textarea>
+
+	</body>
+</html>
