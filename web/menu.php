ffaa24485548d906feb7827f8b2b6184369056ec
Sofian <sofian.ait-ziane@etu.univ-lyon1.fr>
2017-01-11 16:52:17 +0100

    Ajout des fichiers : Blog.php, Metiers.php, Bulletin.php, Informations.php et menu.php. Ces fichiers sont des bases pour continuer le projet.

---
+++ web/menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<table border="1">
+			<TR class="titre">
+				<TH> Accueil </TH> 
+				<TH> Bulletin </TH>
+				<TH> Métiers </TH>
+				<TH> Informations </TH>
+				<TH> Blog </TH>
+			</TR>
+		</table>
+	</body>
+</html>
+
